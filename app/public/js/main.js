d6e9ebe011d54d0c4259c6310f138cd7bfbfcd00
meherrishika <mehernori777@gmail.com>
2021-09-23 13:14:27 -0400

    HW#4 : submit event in vuejs

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -4,24 +4,35 @@ const Person = {
       "person": {},
             }
   },
-  created() {
-      console.log("A");
+  computed :{
+    prettyBirthday() {
+      return dayjs(this.person.dob.date).format('D MMM YYYY');
+    }
 
-      fetch('https://randomuser.me/api/')
-      .then(response => response.json())
-      .then((parsedJson) => {
+  },
+  methods : {
+      fetchUserData(){
+        fetch('https://randomuser.me/api/')
+         .then(response => response.json())
+         .then((parsedJson) => {
           console.log(parsedJson);
           this.person = parsedJson.results[0]
           this.picture = this.person.picture.large
           this.address = this.person.location.street.number + this.person.location.street.name 
           this.userDob = this.person.dob.date[0] + this.person.dob.date[1] +this.person.dob.date[2] +this.person.dob.date[3] + '/' + this.person.dob.date[5] + this.person.dob.date[6] + '/' + this.person.dob.date[8] + this.person.dob.date[9]
-          console.log("C");
-      })
-      .catch( err => {
+         })
+        .catch( err => {
           console.error(err)
-      })
+        })
+      }
 
-      console.log("B");
+  },
+
+
+  created() {
+      this.fetchUserData();
+
+      
   }
 }
 
