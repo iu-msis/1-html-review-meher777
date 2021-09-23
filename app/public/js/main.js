3960519e0878eb40ea299368e9f4683f0d13fa82
meherrishika <mehernori777@gmail.com>
2021-09-23 13:16:13 -0400

    Update main.js

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -19,7 +19,7 @@ const Person = {
           this.person = parsedJson.results[0]
           this.picture = this.person.picture.large
           this.address = this.person.location.street.number + this.person.location.street.name 
-          this.userDob = this.person.dob.date[0] + this.person.dob.date[1] +this.person.dob.date[2] +this.person.dob.date[3] + '/' + this.person.dob.date[5] + this.person.dob.date[6] + '/' + this.person.dob.date[8] + this.person.dob.date[9]
+          #this.userDob = this.person.dob.date[0] + this.person.dob.date[1] +this.person.dob.date[2] +this.person.dob.date[3] + '/' + this.person.dob.date[5] + this.person.dob.date[6] + '/' + this.person.dob.date[8] + this.person.dob.date[9]
          })
         .catch( err => {
           console.error(err)
