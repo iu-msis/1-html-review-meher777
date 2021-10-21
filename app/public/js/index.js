5b9f91824b8202c37db29d2b8bd5d7884b92d60a
meherrishika <mehernori777@gmail.com>
2021-10-20 23:01:15 -0400

    inserting new records into database
    inserting new records into database

---
+++ app/public/js/index.js
@@ -0,0 +1,67 @@
+const Offer = {
+    data() {
+      return {
+            "students": [],
+            "offers": [],
+            "books" :[],
+            "selectedStudent": null
+        }
+    },
+    computed: {
+        // prettyBirthday() {
+        //     return dayjs(this.person.dob.date)
+        //     .format('D MMM YYYY');
+        // }
+    },
+    methods: {
+        /*selectStudent(s) {
+            console.log("Clicked", s);
+            if (this.selectedStudent == s) {
+                return;
+            }
+
+            this.selectedStudent = s;
+            this.offers = [];
+            this.fetchOfferData(s);
+        },
+       /* fetchStudentData() {
+            fetch('/api/student/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.students = parsedJson
+            })
+            .catch( err => {
+                console.error(err)
+            })
+        },*/
+        fetchBooksData() {
+            fetch('/api/books/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.books = parsedJson
+            })
+            .catch( err => {
+                console.error(err)
+            })
+        },
+        /*fetchOfferData(s) {
+            console.log("Fetching offers for", s);
+            fetch('/api/offer/?student=' + s.id)
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.offers = parsedJson
+            })
+            .catch( err => {
+                console.error(err)
+            })
+        }*/
+    },
+    created() {
+        this.fetchBooksData();
+    }
+  }
+  
+Vue.createApp(Offer).mount('#offerApp');
